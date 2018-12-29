10269f85e32b79fcc7057ca1cd33d11098c47045
Reza Sherafat <Pacebox1@gmail.com>
2018-12-29 14:50:04 -0800

    Barebone pypi project.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='gym-tictactoe',
+     version='0.11',
+     scripts=['sayhi'] ,
+     author="Reza Sherafat",
+     author_email="sherafat.us@gmail.com",
+     description="Tic-Tac-Toe environment in OpenAI gym",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/shkreza/gym-tictactoe",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
